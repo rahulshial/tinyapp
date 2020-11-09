57e97b01d5b044ada92622d730524ae6eb294955
Rahul <rahul.shial@gmail.com>
2020-11-09 23:40:16 +0000

    random string generator, POST route

--- express_server.js
+++ express_server.js
@@ -1,7 +1,9 @@
+const generateRandomString = require('./generateRandomString');
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -14,12 +16,24 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   // console.log(req.params);
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const shortURL = generateRandomString();
+  console.log(shortURL);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+ 
+});
+
 app.listen(PORT, () => {
-  console.log(`TinyApp listening on port ${PORT}!`);
+  console.log(`Server Started - TinyApp listening on port ${PORT}!`);
 });
