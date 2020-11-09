7c9bdbbd8cc239144b5a3165f45847379455a3d5
Rahul <rahul.shial@gmail.com>
2020-11-09 16:25:30 +0000

    Server Creation

---
+++ express_server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (request, response) => {
+  response.send("Hello!");
+});
+
+app.get("/urls.json", (request, response) => {
+  response.json(urlDatabase);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
