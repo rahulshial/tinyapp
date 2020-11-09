662994805d8ecf94396683d32098a34ff9b596ab
Rahul <rahul.shial@gmail.com>
2020-11-09 22:56:02 +0000

    updates

--- express_server.js
+++ express_server.js
@@ -9,28 +9,17 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (request, response) => {
-  response.send("Hello!");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
-app.get("/urls.json", (request, response) => {
-  response.json(urlDatabase);
+app.get("/urls/:shortURL", (req, res) => {
+  // console.log(req.params);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+  res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  // res.send("<html><body><h1>Hello <b>World</b></h1></body></html>\n");
-  res.send("<html><body><h1>Hello <b>Rahul</b></h1></body></html>\n");
-});
-
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
-});
- 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
